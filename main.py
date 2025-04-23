6ca63a3a14f451430cb0552ff4d5f12ffc2f82a1
Cody <ckeasli@wgu.edu>
2025-04-22 22:06:28 -0700

    added turtle and made him move in player class

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
 
 game_is_on = True
+screen.listen()
+screen.onkeypress(key='Up',fun=player.move_forward)
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
