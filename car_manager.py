cc31c6a3d74a3b7388b90da7266dae17a0841d25
Cody <ckeasli@wgu.edu>
2025-04-23 21:22:19 -0700

    added blocks to the turtle game and detected collision

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,26 @@
+import random
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.shape('square')
+            new_car.penup()
+            new_car.goto(x=300, y=random.randrange(-240, 240))
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for cars in self.all_cars:
+            cars.backward(STARTING_MOVE_DISTANCE)
+
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+car_manager = CarManager()
 
 game_is_on = True
 screen.listen()
@@ -15,5 +17,13 @@ screen.onkeypress(key='Up',fun=player.move_forward)
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
 
 screen.exitonclick()
